6095c94ed68ad95b41801fa230fdc1196361490a
Yourset <775490213@qq.com>
2021-01-19 00:10:29 +0800

    a星管理器2

--- Assets/astar_node.cs
+++ Assets/astar_node.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,10 +15,33 @@ public class point
     public point start_point;
     //终点
     public point end_point;
+    
+    //是否是障碍物
+    public bool is_barrier = false;
 }
 
 public class astar_node:MonoBehaviour
 {
-    
+    point point = new point();
+    private void Start()
+    {
+        
+    }
+
+    //当鼠标按下的时候 进行是否变成障碍物
+    private void OnMouseDown()
+    {
+        //如果本来是 就变成 不是 反正就是一个取反的工作
+        point.is_barrier = !point.is_barrier;
+        if (point.is_barrier == true)
+        {
+            this.transform.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            this.transform.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+    }
 }
 
