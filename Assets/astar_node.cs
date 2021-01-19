dbf9c4d38839c8eea1ff8b61385c2add10ada2b5
Yourset <775490213@qq.com>
2021-01-19 15:45:46 +0800

    openlist

--- Assets/astar_node.cs
+++ Assets/astar_node.cs
@@ -1,36 +1,59 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 //建立节点所拥有的属性
 public class point
 {
+    
     //父亲节点
-    public point parent;
+    public point parent=null;
     //全局管理器的加入
-    public manager manager;
+    public manager manager=null;
 
     //起点
-    public point start_point;
+    public point start_point=null;
     //终点
-    public point end_point;
+    public point end_point=null;
+    //x坐标和y坐标
+    
+    public int x=0;
+    public int y=0;
+
+    public double consume=999;
     
     //是否是障碍物
     public bool is_barrier = false;
+
+    /**
+     * 计算当前方格的消耗
+     */
+    public void caculate_consume()
+    {
+        double distance_to_staert;
+        // distance_to_staert = x-
+        consume = Math.Pow(x * x + y * y, 0.5f);
+        // consume = consume+
+    }
+    
+    
 }
 
 public class astar_node:MonoBehaviour
 {
-    point point = new point();
-    private void Start()
+    public point point = new point();
+    private void Start ()
     {
+        point = new point();
         
     }
 
     //当鼠标按下的时候 进行是否变成障碍物
     private void OnMouseDown()
     {
+        Debug.Log("按下");
         //如果本来是 就变成 不是 反正就是一个取反的工作
         point.is_barrier = !point.is_barrier;
         if (point.is_barrier == true)
